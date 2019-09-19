a5c22f1e73cd20b0e5307d453840bf6e349a5c99
Hudson Noll <legohudmo@gmail.com>
2019-09-19 12:08:13 -0400

    Hello-world commit

---
+++ noll_p2/src/noll_p2/Main.java
@@ -0,0 +1,41 @@
+package noll_p2;
+import java.util.Scanner;
+
+public class Main {
+
+	//calculate body mass index
+	public static void bodyMassIndex (String [] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Type '1' for BMI calculator in pounds and inches, "
+				+ "or 2 for kilograms and meters");
+		int userNum = scnr.nextInt();
+		double BMI = 0.0;
+		
+		if (userNum == 1) {
+			double weightInLbs = scnr.nextDouble();
+			double heightInInches = scnr.nextDouble();
+			//Scanner scnr = new Scanner(System.in);
+			BMI = (703 * weightInLbs) / (Math.pow(heightInInches,2));
+		}
+			
+		if (userNum == 2) {
+			double weightInKilos = scnr.nextDouble();
+			double heightInMeters = scnr.nextDouble();
+			//Scanner scnr = new Scanner(System.in);
+			//$$BMI\:=\:frac{weightInKilos}{heightInMeters^2}$$;
+			BMI = weightInKilos / (Math.pow(heightInMeters,2)); 
+		}
+		
+		System.out.print("Your Body Mass Index is: ");
+		System.out.printf("%.1f", BMI);
+		System.out.println("According to the National Heart, Lung, and Blood Institute,"
+				+ "BMI categories range from: ");
+		System.out.println("Underweight < 18.5");
+		System.out.println("18.5 <= Normal Weight <= 24.9");
+		System.out.println("25 <= Overweight <= 29.9");
+		System.out.println("Obeisity >= 30");
+		System.out.println("Source: https://nhlbi.nih.gov/health/educational/lose_wt/BMI/"
+			+ "bmicalc.htm");
+	}
+}
